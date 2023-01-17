9193e52c8fedb8e6eafdb427a0a093280b3decb8
griimreaper <leonelbehnke@gmail.com>
2023-01-17 13:59:31 -0300

    ExtraHomeworks Done

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,6 +6,10 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+   var array = []
+   for(key in objeto){
+      array.push([key, objeto[key]])
+   }return array
 }
 
 function numberOfCharacters(string) {
@@ -14,6 +18,12 @@ function numberOfCharacters(string) {
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
+   var objeto = {}
+   var letras = string.split("")
+   for(i=0;i<letras.length;i++){
+   var cantidad =letras.filter(letra => letra == letras[i])
+   objeto[letras[i]] = cantidad.length
+}return objeto
 }
 
 function capToFront(string) {
@@ -22,6 +32,15 @@ function capToFront(string) {
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
    // Tu código:
+var mayúscula = [] 
+var minuscula = []
+var letras = string.split("")
+for(i=0;i<letras.length;i++){
+   if(letras[i] == letras[i].toUpperCase()){mayúscula.push(letras[i])}
+   else {minuscula.push(letras[i])}
+}
+var union = mayúscula.join('')+ minuscula.join('')
+return union
 }
 
 function asAmirror(frase) {
@@ -29,18 +48,43 @@ function asAmirror(frase) {
    // La diferencia es que cada palabra estará escrita al inverso.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
    // Tu código:
+   var reverse = []
+   var letra = []
+   var palabras = frase.split("")
+   palabras.reverse()
+   var union = palabras.join("")
+   let espacio = union.split(" ")
+   for(i=0;i<espacio.length;i++){
+      reverse.unshift(espacio[i])
+   }
+   var final = reverse.join(" ")
+   return final
 }
 
 function capicua(numero) {
    // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
    // Caso contrario: "No es capicua".
    // Tu código:
+   var capicua = numero.toString().split("").reverse().join("");
+   
+   if(numero==capicua){
+      return "Es capicua"
+   }else {return "No es capicua"}
 }
 
 function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
    // Retorna el string sin estas letras.
    // Tu código:
+   var newarray = []
+   var letras = string.split("")
+   for(i=0;i<letras.length;i++){
+      if(letras[i] != "a"&& letras[i] != "b"&&letras[i] !="c"){
+      newarray.push(letras[i])
+   }
+   var final = newarray.join("")
+   }
+   return final
 }
 
 function sortArray(arrayOfStrings) {
@@ -49,6 +93,20 @@ function sortArray(arrayOfStrings) {
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
    // Tu código:
+   var arreglo = arrayOfStrings
+   var save_I = null
+   var save_j= null
+   for(i=0; i<arreglo.length - 1; i++){
+      for(j=i+1; j<arreglo.length; j++){
+         if(arreglo[i].length>arreglo[j].length){
+            save_I= arreglo[i]
+            save_j= arreglo[j]
+            arreglo[i] = save_j
+            arreglo[j] = save_I
+         }
+      }
+   }
+   return arreglo
 }
 
 function buscoInterseccion(array1, array2) {
@@ -58,6 +116,24 @@ function buscoInterseccion(array1, array2) {
    // Si no tienen elementos en común, retornar un arreglo vacío.
    // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
    // Tu código:
+   let newArray = [];
+   let cantidadMay = [];
+   let cantidadMen = [];
+
+   if (array1 > array2) {
+      cantidadMay = array1;
+      cantidadMen = array2;
+   } else {
+      cantidadMay = array2;
+      cantidadMen = array1;
+   }
+
+   for (let x = 0; x < cantidadMay.length - 1; x++) {
+      if (cantidadMay.includes(cantidadMen[x])) {
+      newArray.push(cantidadMen[x]);
+      }
+   }
+   return newArray;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
